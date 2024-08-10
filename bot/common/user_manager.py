86e56134dcb434825c949ab6ea4ae56b0eafc288
diartor <mhmmd2020.15@gmail.com>
2024-08-10 16:14:34 +0330

    Added is_private_chat + user now must join multiple channels, or just one

--- bot/common/user_manager.py
+++ bot/common/user_manager.py
@@ -5,11 +5,21 @@ from telebot.apihelper import ApiTelegramException
 
 
 def is_user_in_channel(user_id, bot: telebot.TeleBot):
+    joined_in = 0
+    channels = len(get_settings_data("channels_id"))
     for channel_id in get_settings_data("channels_id"):
-        # Check if the user is a member, administrator, or creator
         try:
             member = bot.get_chat_member(channel_id, user_id)
             if member.status in ['member', 'administrator', 'creator']:
-                return True
+                joined_in += 1
+                if joined_in == channels:
+                    return True
         except ApiTelegramException:
             print(f"{channel_id} Telegram Chat ID Is Incorrect ")
+
+
+def is_private_chat(msg: telebot.types.Message, bot: telebot.TeleBot):
+    if msg.chat.type != "private":
+        bot.reply_to(msg, "please PM me if you want to talk to me :)")
+        return False
+    return True
